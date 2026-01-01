0a44fa12c9bfd95239dc444a4c541e56be9232dd
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2026-01-01 15:57:31 -0800

    The animation isn't good, but i added collision to rocks, and lets player look in a direction even if they aren't going to go in that direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,17 +14,31 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_pressed("up") and !$up.is_colliding():
 			anim_sprite.play("walk_back")
 			move(Vector2(0, -1))
+		elif Input.is_action_pressed("up"):
+			anim_sprite.play("up")
+			
 		elif Input.is_action_pressed("down") and !$down.is_colliding():
+			anim_sprite.play("down")
 			anim_sprite.play("walk_front")
 			move(Vector2(0, 1))
+		elif Input.is_action_pressed("down"):
+			anim_sprite.play("down")
 		elif Input.is_action_pressed("left") and !$left.is_colliding():
 			anim_sprite.flip_h = true
+			anim_sprite.play("right")
 			anim_sprite.play("walk_right")
 			move(Vector2(-1, 0))
+		elif Input.is_action_pressed("left"):
+			anim_sprite.flip_h = true
+			anim_sprite.play("right")
 		elif Input.is_action_pressed("right") and !$right.is_colliding():
 			anim_sprite.flip_h = false
+			anim_sprite.play("right")
 			anim_sprite.play("walk_right")
 			move(Vector2(1, 0))
+		elif Input.is_action_pressed("right"):
+			anim_sprite.flip_h = false
+			anim_sprite.play("right")
 	 
 func move(dir: Vector2):
 	global_position += dir * tile_size
