f76ad2fa6f2540941ea756f03998f6aae16939f0
UnstoppableBlob <96858668+UnstoppableBlob@users.noreply.github.com>
2026-01-01 15:11:30 -0800

    timed the walking

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,7 @@ var sprite_node_pos_tween: Tween
 func _physics_process(delta: float) -> void:
 	if !sprite_node_pos_tween or !sprite_node_pos_tween.is_running():
 		if Input.is_action_pressed("up") and !$up.is_colliding():
+			anim_sprite.play("walk_back")
 			move(Vector2(0, -1))
 		elif Input.is_action_pressed("down") and !$down.is_colliding():
 			move(Vector2(0, 1))
@@ -28,7 +29,7 @@ func move(dir: Vector2):
 		sprite_node_pos_tween.kill()
 	sprite_node_pos_tween = create_tween()
 	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	sprite_node_pos_tween.tween_property(anim_sprite, "global_position", global_position, 0.185).set_trans(Tween.TRANS_LINEAR)
+	sprite_node_pos_tween.tween_property(anim_sprite, "global_position", global_position, 0.25).set_trans(Tween.TRANS_LINEAR)
 	
 	
 	
